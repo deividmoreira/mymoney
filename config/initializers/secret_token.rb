e00b44ed4449132ca3957a47716ed6d4c6f278d1
deivid <moreira.deivid@gmail.com>
2011-09-28 14:39:59 -0300

    adicionando a estrutura do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymoney::Application.config.secret_token = 'ff59009de59f55a47c8d25e22f54dd5b41c6d13c3065d0e2ed17c13590f898d8eaf2e0a2c79c28ef10bd98b0f35d9905c5c44c365e7f7fef9177e68fa5fc1b1b'
